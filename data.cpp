9939b858e3b4700e8f52d01fe856c68f67c5330a
Ramilbey <ramilbey505711503@gmail.com>
2025-04-09 03:07:12 +0800

    find the max num done

--- data.cpp
+++ data.cpp
@@ -2,8 +2,15 @@
 using namespace std; 
 
 int main(){
-    cout << "hello world" << endl;
-    cout << "i like pizza";
-    return 0;
+    // Write a program to find the maximum element out of total three.
+    int a = 10;
+    int b = 20;
+    int c = 90;
 
+    if(a > b && a >c)
+        cout << "max is "<< a << endl;
+    else if(b > a && b > c)
+        cout << "max is " << b << endl;
+    else
+        cout << "max is " << c << endl;
 }
